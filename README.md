b64ba02995acf03fc44b9caeb9ec4ef4aa25a6a5
camilopv19 <kmilopv@gmail.com>
2021-01-15 21:13:49 -0500

    Test

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Front-end test for Palo IT position
+
+  - Validate that the number has between 14 and 16 digits, if the validation is not successful, show an alert to the user
+  - Print in the Log, the number of times a number is repeated
+  - In the Label field below the button, show which number is repeated the most times
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PaloIT';
+
+  formulario = this.fb.group({
+    digito: ['', Validators.required],
+    num: ''
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  validar() {
+    let digito = this.formulario.value.digito;
+
+    if (this.validarLongitud(digito)) {
+      let arr = digito.toString().split('');
+  
+      //Quitar duplicados y contar recurrencia
+      let noDups = this.quitarDuplicados(arr);
+  
+      //Actualizar el template con el número de mayor recurrencia
+      this.formulario.patchValue({
+        num: this.elegirMaximo(noDups)
+      })
+    }
+  }
+
+  validarLongitud(num){
+    
+    if ( num.length < 14 || num.length > 16 ) {
+      alert( `El número debe tener entre 14 y 16 dígitos.\nEl número ingresado tiene ${num.length}`)
+      return false;
+    }
+    return true;
+  }
+
+  quitarDuplicados(arr) {
+    let counts = {};
+
+    for (let i = 0; i < arr.length; i++) {
+      let num = arr[i];
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    console.log('Repeticiones: ', counts);
+    return counts;
+  }
+
+  elegirMaximo(arr) {
+    let baseNum = -1;
+    let luckyNum;
+
+    Object.keys(arr).forEach(e => {
+      if (arr[e] > baseNum) {
+        baseNum = arr[e];
+        luckyNum = e;
+      }
+    });
+    return luckyNum;
+  }
 }
